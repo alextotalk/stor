891cfb9a0d20302679add8f2337dce05e67522dd
alextotalk <alexoo@inbox.ru>
2024-10-10 15:36:31 +0300

    Додав main.go

--- cmd/stor/main.go
+++ cmd/stor/main.go
@@ -2,15 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World!")
+func handler(w http.ResponseWriter, _ *http.Request) {
+	if _, err := fmt.Fprintf(w, "Hello, World!"); err != nil {
+		log.Printf("Failed to write response: %v", err)
+	}
 }
 
 func main() {
 	http.HandleFunc("/", handler)
 	fmt.Println("Server is running on port 8080")
-	http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatalf("Server failed to start: %v", err)
+	}
 }
